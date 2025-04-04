f69da5bb0849f222cd137efd558806733759e51d
Proxypepe <37917466+Proxypepe@users.noreply.github.com>
2025-04-04 12:30:37 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,13 +7,22 @@ terraform {
 }
 
 provider "sonarqube" {
-  host = ""
+  host = "http://"
   token = ""
 }
 
 module "app_team_groups" {
   source        = "./modules/sonarqube_groups_with_template"
-  group_base_name = "app_team"
+  admin_group = "app_team_admin"
+  view_group = "app_team_view_execute"
+  project = "app_team"
   description   = "Groups and permissions for Application Team"
 }
 
+module "infra_team_groups" {
+  source        = "./modules/sonarqube_groups_with_template"
+  admin_group = "infra_team_admin"
+  view_group = "infra_team_view_execute"
+  project = "infra_team"
+  description   = "Groups and permissions for Application Team"
+}
